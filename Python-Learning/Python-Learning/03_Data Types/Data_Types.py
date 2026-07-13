efc4e2588deae131f07ce5c9ceaa5a7a09e3916a
Molla-Tesfay <mollatesfsy@hotmail.com>
2026-07-13 17:42:54 +0100

    Add files via upload

---
+++ Python-Learning/Python-Learning/03_Data Types/Data_Types.py
@@ -0,0 +1,33 @@
+age = 28
+goals =15
+cars = 3
+
+print(age)
+height = 1.65
+temperature = 23.5
+print(height)
+name = "Molla"
+country = "Eritrea"
+Course = "python"
+print(name)
+is_student = True
+print(is_student)
+name = "Molla"
+age = 28
+height = 1.65
+student = True
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(student))
+age = "28"
+number = int(age)
+print(type(number))
+age = 28
+hight = float(age)
+print(hight)
+print(type(50))
+print(type("50"))
+print(type(5.5))
+print(type(False))
+print("20" + "10")
